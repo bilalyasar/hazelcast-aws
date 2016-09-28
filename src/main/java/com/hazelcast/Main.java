43fd14b93ed9a2791bc18b6025ee92a68bb9d00a
bilalyasar <bilalyasar45@gmail.com>
2016-09-28 12:44:15 +0300

    wip

---
+++ src/main/java/com/hazelcast/Main.java
@@ -0,0 +1,12 @@
+package com.hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+
+/**
+ * Created by bilal on 9/28/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hazelcast.newHazelcastInstance();
+    }
+}
